ea7cdf4465a61bda46961d4fe895c855affbd67f
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-31 11:00:09 +0600

    data model creted for new user

---
+++ src/app/modules/users/user.model.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+import { IUser } from "./user.interface";
+
+const userModel = new Schema<IUser>({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user']
+    }
+});
+
+const User = mongoose.model<IUser>('user', userModel);
+export default User;
