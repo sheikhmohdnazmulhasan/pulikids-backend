2b9400c6e0d5f9c0f1039cfdd004bf177600de86
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-31 21:38:07 +0600

    validated user registration body

--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { UserController } from "./users.controller";
+import ValidationRequest from "../../middlewares/zod_validation";
+import { UserValidation } from "./user.validation";
 
 const router = Router();
 
-router.post('/register', UserController.createUser);
+router.post('/register', ValidationRequest(UserValidation.createUserValidation), UserController.createUser);
 router.post('/login', UserController.loginUser);
 router.post('/recovery', UserController.resetPassword);
 
--- src/app/modules/users/user.validation.ts
+++ src/app/modules/users/user.validation.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 
 const createUserValidation = z.object({
-    firstName: z.string().nonempty({ message: "First name is required" }),
-    lastName: z.string().nonempty({ message: "Last name is required" }),
-    email: z.string().email({ message: "Invalid email address" }),
-    role: z.enum(['admin', 'user']).optional(),
+    body: z.object({
+        firstName: z.string().nonempty({ message: "First name is required" }),
+        lastName: z.string().nonempty({ message: "Last name is required" }),
+        email: z.string().email({ message: "Invalid email address" }),
+        password: z.string().nonempty({ message: "password is required" }),
+    })
 });
 
 export const UserValidation = {
