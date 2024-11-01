0ff303ea422e8f9ed709d3bf0e6ed1184aa09173
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-02 00:21:00 +0600

    validation booking data

--- src/app/modules/bookings/booking.route.ts
+++ src/app/modules/bookings/booking.route.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { BookingController } from "./booking.controller";
 import Auth from "../../middlewares/auth";
 import { userRole } from "../../constants/constant.user.role";
+import ValidationRequest from "../../middlewares/zod_validation";
+import { BookingValidation } from "./booking.validation";
 
 const router = Router();
 
 router.post('/create-booking',
     Auth([userRole.USER]),
+    ValidationRequest(BookingValidation.createBookingSchemaValidation),
     BookingController.createBooking);
 
 export const BookingRoutes = router;
--- src/app/modules/bookings/booking.validation.ts
+++ src/app/modules/bookings/booking.validation.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
 
 export const createBookingSchemaValidation = z.object({
-    activityId: z.string().regex(/^[0-9a-fA-F]{24}$/, 'Invalid Activity ID'), // Checks for a valid ObjectId
-    userId: z.string().regex(/^[0-9a-fA-F]{24}$/, 'Invalid User ID'), // Checks for a valid ObjectId
-    bookingDate: z.date(),
+    body: z.object({
+        activityId: z.string().regex(/^[0-9a-fA-F]{24}$/, 'Invalid Activity ID'), // Checks for a valid ObjectId
+        // userId: z.string().regex(/^[0-9a-fA-F]{24}$/, 'Invalid User ID'), // Checks for a valid ObjectId
+        bookingDate: z.string().transform((str) => new Date(str)).optional(),
+    })
 });
 
 export const BookingValidation = {
